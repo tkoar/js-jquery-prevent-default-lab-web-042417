fcb2390faf25cd47e766878be0ec87a7fa48851c
tkoar <terrancekoar@gmail.com>
2017-06-12 09:20:51 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
+
 $(document).ready(function(){
-  // call functions here
+
+  submitForm();
 });
 
-// define functions here
+function submitForm(){
+  $('form').on('submit', function(event){
+    var listItem = $('#item').val();
+    $('ol').append( '<li>' + listItem + '</li>' );
+    event.preventDefault();
+  });
+}
